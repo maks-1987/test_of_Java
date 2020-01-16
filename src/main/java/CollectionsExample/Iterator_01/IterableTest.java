d95875de98cf54998ea8ae9cab480a2823b619fa
MaxD <max.moderation@gmail.com>
2020-01-16 20:23:38 +0200

    Add Iterator

--- src/main/java/CollectionsExample/Iterator_01/IterableTest.java
+++ src/main/java/CollectionsExample/Iterator_01/IterableTest.java
@@ -1,6 +1,7 @@
 package CollectionsExample.Iterator_01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class IterableTest {
@@ -10,8 +11,19 @@ public class IterableTest {
         integerList.add(2);
         integerList.add(3);
 
-        for (int x : integerList) {
-            System.out.println(x);
+        Iterator<Integer> iterator = integerList.iterator();
+        /** Только тут можем удалять элем во время итерации */
+        int count = 0;
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            if (count == 1) iterator.remove(); // не приним аргум, т.к. проходя по списку можем удалить только текущ элемент
+            count++;
         }
-     }
+        System.out.println(integerList);
+
+        for (int x : integerList) {
+            //integerList.remove(1); // Exception in thread "main" java.util.ConcurrentModificationException
+            System.out.println("\t" + x);
+        }
+    }
 }
